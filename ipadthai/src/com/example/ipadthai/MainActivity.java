73bd34965ba3f89e0765b5d33ffc3b41b35f8fb7
guillaumevalverde <guillaume.valverde@orange.com>
2012-12-21 11:45:38 +0000

    Plate Ready

--- ipadthai/src/com/example/ipadthai/MainActivity.java
+++ ipadthai/src/com/example/ipadthai/MainActivity.java
@@ -3,19 +3,21 @@ package com.example.ipadthai;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.widget.ImageView;
 
 public class MainActivity extends Activity {
 	
 	
 	
 	private Plate mPlate = null;
+	private ImageView[] listImage = null;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         
-        mPlate = new Plate(10,4,3,this);
+        mPlate = new Plate(10,4,3,5,6,this);
         for(int i = 0; i<mPlate.mNumElement;i++){
         	
         }
--- ipadthai/src/com/example/ipadthai/Plate.java
+++ ipadthai/src/com/example/ipadthai/Plate.java
@@ -13,8 +13,11 @@ public class Plate {
 	private final int mNumberPrawnVariety =4; 
 	private final int mNumberCorianderVariety =4; 
 	private int mNumberPate = 0;
-	private int mNumberPiment = 0;
-	private int mNumberCorriandre = 0;
+	private int mNumberOnion = 0;
+	private int mNumberChicken = 0;
+	private int mNumberPrawn = 0;
+	private int mNumberCoriander = 0;
+	
 	private Context context;
 	private final int  width =300;
 	private final int height = 300;
@@ -25,43 +28,34 @@ public class Plate {
 	public int mNumElementEaten= 0;
 	public int mNumElement = 0;
 	
-	private int[] ressourcePate = new int[4];
-	private int[] ressourcePiment = new int[4];
-	private int[] ressourceCoriandre = new int[4];
-	
-	public Plate(int numPate, int numPiment, int numCoriander, Context context){
+	public Plate(int numPate, int numOnion,int numChicken,int numPrawn, int numCoriander, Context context){
 		this.context = context;
 		mNumberPate = numPate;
-		mNumberCorriandre = numCoriander;
-		mNumberPiment = numPiment;
-		mNumElement = mNumberPate+ mNumberCorriandre + mNumberPiment;
+		mNumberOnion = numOnion;
+		mNumberChicken = numChicken;
+		mNumberPrawn = numPrawn;
+		mNumberCoriander = numCoriander;
+		mNumElement = mNumberPate+ mNumberOnion + mNumberChicken+mNumberPrawn+mNumberCoriander;
 		
-		
-		ressourcePate[0] = R.drawable.pate1;
-		ressourcePate[1] = R.drawable.pate1;
-		ressourcePate[2] = R.drawable.pate2;
-		ressourcePate[3] = R.drawable.pate2;
-		ressourcePiment[0] = R.drawable.pate1;
-		ressourcePiment[1] = R.drawable.pate1;
-		ressourcePiment[2] = R.drawable.pate2;
-		ressourcePiment[3] = R.drawable.pate2;
-		ressourceCoriandre[0] = R.drawable.pate1;
-		ressourceCoriandre[1] = R.drawable.pate1;
-		ressourceCoriandre[2] = R.drawable.pate2;
-		ressourceCoriandre[3] = R.drawable.pate2;
 		
 		for(int i=0 ; i<mNumElement;i++){
 			mXCoord.add((int) (Math.random()*width));
 			mYCoord.add((int) (Math.random()*height));
 			
 			if(i<mNumberPate){
-				mRefImage.add( ressourcePate[ (int) Math.random()*ressourcePate.length    ] );
+				mRefImage.add( context.getResources().getIdentifier("pate0"+(int)Math.random()*mNumberPateVariety, "drawable", context.getPackageName()) );
 			}
-			else if(i>=mNumberPate&& i<mNumberPiment){
-				mRefImage.add( ressourcePiment[ (int) Math.random()*ressourcePiment.length    ] );
+			else if(i>=mNumberPate&& i<mNumberOnion){
+				mRefImage.add( context.getResources().getIdentifier("onion0"+(int)Math.random()*mNumberOnionVariety, "drawable", context.getPackageName()) );
 			}
-			else if(i>=mNumberPiment&& i<mNumberCorriandre){
-		//		mRefImage.add( context.getResources().getIdentifier("chicken"+, "drawable", context.getPackageName()))) );
+			else if(i>=mNumberOnion&& i<mNumberChicken){
+				mRefImage.add( context.getResources().getIdentifier("chicken0"+(int)Math.random()*mNumberChickenVariety, "drawable", context.getPackageName()) );
+			}
+			else if(i>=mNumberChicken&& i<mNumberPrawn){
+				mRefImage.add( context.getResources().getIdentifier("prawn0"+(int)Math.random()*mNumberPrawnVariety, "drawable", context.getPackageName()) );
+			}
+			else if(i>=mNumberPrawn&& i<mNumberCoriander){
+				mRefImage.add( context.getResources().getIdentifier("coriander0"+(int)Math.random()*mNumberCorianderVariety, "drawable", context.getPackageName()) );
 			}
 		}
 		
@@ -84,5 +78,8 @@ public class Plate {
 	public int getYCoordElement(int i){
 		return mYCoord.get(i);
 	}
-
+	public int getRefElement(int i){
+		return mRefImage.get(i);
+	}
+	
 }
